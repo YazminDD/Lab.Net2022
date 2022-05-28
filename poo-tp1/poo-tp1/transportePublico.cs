cb0674b277efbfb67d83886cba523c2c24817446
Franfdd <franco.dedona@outlook.com>
2022-05-28 16:30:16 -0300

    se agregó clase transporte público

---
+++ poo-tp1/poo-tp1/transportePublico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_tp1
+{
+    public class transportePublico
+    {
+        private int pasajeros;
+
+        public void Avanzar()
+        {
+        }
+        public void Detenerse()
+        {
+        }
+     }
+}
