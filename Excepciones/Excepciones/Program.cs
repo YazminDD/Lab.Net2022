1f7302761c06fb1e85019e9405eb5ad66161bdbe
YazminDD <106122833+YazminDD@users.noreply.github.com>
2022-05-31 20:01:33 -0300

    Ejercicio 2

--- Excepciones/Excepciones/Program.cs
+++ Excepciones/Excepciones/Program.cs
@@ -36,6 +36,49 @@ namespace Excepciones
                 Console.WriteLine("La división finalizó");
                 Console.ReadKey();
             }
+
+            // Ejercicio 2
+
+            try
+            {
+                Console.WriteLine("------------------------------------------- ");
+                Console.WriteLine(" ");
+                Console.WriteLine("Ejercicio 2 División");
+                Console.WriteLine(" ");
+                Console.WriteLine("Ingrese el primer número");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo número");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                resultadoDivision = Operacion.Dividir(num1, num2);
+                Console.WriteLine("El resultado es : " + resultadoDivision);
+            }
+            catch (DivideByZeroException ms)
+            {
+                Console.WriteLine(ms.Message);
+                Console.WriteLine("¡Solo Chuck Norris divide por cero!");
+                
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("¡Seguro ingreso una letra o no ingresó nada!");
+                
+            }
+
+            catch (Exception ms)
+            {
+                Console.WriteLine("Otros errores : " + ms.Message);
+                
+            }
+
+            finally
+            {
+                Console.WriteLine("La división finalizó");
+                Console.ReadKey();
+            }
+
+            // Ejercicio 3
+
         }
     }
 }
