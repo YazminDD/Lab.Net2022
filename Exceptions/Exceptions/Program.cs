555be246fb146e9e3633a3723451db9b5da6a545
YazminDD <106122833+YazminDD@users.noreply.github.com>
2022-05-31 17:32:51 -0300

    1

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            // Ejercicio 1
+
+            Console.WriteLine("Ejercicio 1 División");
+            Console.WriteLine(" ");
+
+            try
+            {
+                
+                Console.WriteLine("El resultado es ");
+            }
+            catch (DivideByZeroException ms)
+            {
+                Console.WriteLine(ms.Message);
+            }
+            finally
+            {
+                Console.WriteLine("La división finalizó");
+                Console.ReadKey();
+            }
+            
+
+            // Ejercicio 2
+
+
+        }
+
+
+    }
+}
+
