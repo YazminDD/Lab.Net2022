038f091af4bdea705d588521f29d0fb6d850a263
YazminDD <106122833+YazminDD@users.noreply.github.com>
2022-06-08 01:30:39 -0300

    Puntos 1, 2 y 3

---
+++ EntityFramework/EntityFramework.Data/NorthwindContext.cs
@@ -0,0 +1,26 @@
+using EntityFramework.Entities;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EntityFramework.Data
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+            : base("name=NorthwindConnection")
+        {
+        }
+
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Shippers> Shippers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+        }
+    }
+}
---
+++ EntityFramework/EntityFramework.Entities/Shippers.cs
@@ -0,0 +1,21 @@
+namespace EntityFramework.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    
+
+    public partial class Shippers
+    {
+        [Key]
+        public int ShipperID { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CompanyName { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+    }
+}
