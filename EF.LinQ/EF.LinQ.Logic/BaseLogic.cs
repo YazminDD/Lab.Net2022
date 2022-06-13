052a0be777ac53ab62983a7932e231a9270e0c33
YazminDD <106122833+YazminDD@users.noreply.github.com>
2022-06-12 21:14:37 -0300

    Creación de NorthwindContext, Entities, Base Logic y IAMBLogic

---
+++ EF.LinQ/EF.LinQ.Logic/BaseLogic.cs
@@ -0,0 +1,18 @@
+﻿using EF.LinQ.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.LinQ.Logic
+{
+    public class BaseLogic
+    {     
+        protected readonly NorthwindContext context;
+        public BaseLogic()
+        {
+           context = new NorthwindContext();
+        }
+    }
+}
